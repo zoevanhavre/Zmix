526b95b70c8c2cc5bbfda244c8af2318a89f5083
Zoe van Havre <zoevanhavre@gmail.com>
2015-12-11 14:30:13 +1000

    make a single plot to show mcmc
    To make output less busy.

--- R/Zmix_univ_tempered.R
+++ R/Zmix_univ_tempered.R
@@ -117,12 +117,12 @@ Zmix_univ_tempered<-function(y, k=10,iter=5000,  tau=0.01, isSim=FALSE, alphas= 
 
 					Sys.sleep(0.01)
 					setTxtProgressBar(pb, j)
-					if(j %% 10==0){
-					par(mfrow=c(1,3))
-					plot(SteadyScore$K0~SteadyScore$Iteration, main='#non-empty groups', type='l')
-					ts.plot(Bigp[[nCh]], main='Weights from target posterior', col=rainbow(k))
-					ts.plot(TrackParallelTemp[,c(nCh:1)], main='Track Parallel Tempering', col=rainbow(nCh))
-						Sys.sleep(0)}
+					# if(j %% 10==0){
+					# par(mfrow=c(1,3))
+					# plot(SteadyScore$K0~SteadyScore$Iteration, main='#non-empty groups', type='l')
+					# ts.plot(Bigp[[nCh]], main='Weights from target posterior', col=rainbow(k))
+					# ts.plot(TrackParallelTemp[,c(nCh:1)], main='Track Parallel Tempering', col=rainbow(nCh))
+					# 	Sys.sleep(0)}
 
 					for (.ch in 1:nCh){
 			                                                                                                          #################
@@ -228,8 +228,16 @@ Zmix_univ_tempered<-function(y, k=10,iter=5000,  tau=0.01, isSim=FALSE, alphas= 
 					SteadyScore$K0[j]<-sum(table(ZSaved[[nCh]][,j])>0)
 
 					}
-
+					
 					close(pb)
+
+					# make at least one plot showing target
+					Sys.sleep(1)
+					par(mfrow=c(1,3))
+					plot(SteadyScore$K0~SteadyScore$Iteration, main='#non-empty groups', type='l')
+					ts.plot(Bigp[[nCh]], main='Weights from target posterior', col=rainbow(k))
+					ts.plot(TrackParallelTemp[,c(nCh:1)], main='Track Parallel Tempering', col=rainbow(nCh))
+				
 					#SteadyScore<-unlist(lapply(apply(ZSaved[[nCh]], 2, table), length))
 					BigRes<-list(Bigmu = Bigmu, Bigsigma=Bigsigma, Bigp = Bigp, Loglike=Loglike, Zs=ZSaved, YZ=y, SteadyScore=SteadyScore,TrackParallelTemp=TrackParallelTemp)
 					#SmallRes<-trimit(BigRes,  nEnd=EndSize)
