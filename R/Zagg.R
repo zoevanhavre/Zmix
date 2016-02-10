8dcbb60886e99f988e992cdf83a71da50ce4c882
Zoe van Havre <zoevanhavre@gmail.com>
2016-02-10 13:26:21 +1000

    Merging updated Rd files

--- R/Zagg.R
+++ R/Zagg.R
@@ -1,32 +1,28 @@
 #' Processing output of label switching function
 #'
-#' explain here
-#' @param output of UNIVARIATE label switching function
+#' Used inside Post Processing function
+#' @param USout: output of Zswitch
+#' @param .Y: Y, vector of original data
 #' @keywords postprocessing
 #' @export
 #' @examples
-#' #nope
+#' #NA
 
 
 Zagg<-	function(USout, .Y=Y){
-		
-			#Pars
-				.par<-melt(USout$Pars, id.vars=c("Iteration", "k"))
-				theta<-aggregate( value~variable+factor(k), mean ,data=.par)
-	         	K<-max(.par$k)
-			
-						#Pred		
-				maxZ<-function (x)  as.numeric(names(which.max(table( x ))))
-	       		Zhat<- factor( apply(t(USout$Zs), 2,maxZ))
-	       	 	#cbind(Zhat,USout$YZ$Y, USout$YZ$Z )   
-	         	#RAND<-sum(USout$YZ$Z==Zhat)/length(Zhat)*100    
-	         		# MSE
-         			Zemu<-as.numeric(Zhat)
-         			.Mus<-theta$value[theta$variable=="Mu"]
-	         		for ( i in 1:length(Zemu)){
-	         			Zemu[i]<-.Mus[as.numeric(Zhat[i])]}
-	         	MSE<-sum((.Y-Zemu)^2)
-	         	MAE<-sum(abs(.Y-Zemu))
+	#Parameters
+	.par<-melt(USout$Pars, id.vars=c("Iteration", "k"))
+	theta<-aggregate( value~variable+factor(k), mean ,data=.par)
+ 	K<-max(.par$k)			
+	#Pred
+	maxZ<-function (x)  as.numeric(names(which.max(table( x ))))
+	Zhat<- factor( apply(t(USout$Zs), 2,maxZ))
+	Zemu<-as.numeric(Zhat)
+	.Mus<-theta$value[theta$variable=="Mu"]
+	for ( i in 1:length(Zemu)){
+		Zemu[i]<-.Mus[as.numeric(Zhat[i])]}
+	 MSE<-sum((.Y-Zemu)^2)
+	 MAE<-sum(abs(.Y-Zemu))
 
-	       		list("theta"=theta,  "Zpred"=Zhat, "MSE"=MSE, "MAE"=MAE)
-					}
+			list("theta"=theta,  "Zpred"=Zhat, "MSE"=MSE, "MAE"=MAE)
+			}
